6c3fb9c9dacdf5bb05d9e5a17087d35aca024780
Robert Rumney <robertrumney@gmail.com>
2023-04-09 11:03:31 +0200

    Update EasterEgg.cs

--- EasterEgg.cs
+++ EasterEgg.cs
@@ -3,6 +3,37 @@ using UnityEngine;
 // Basic example egg class
 public class EasterEgg : MonoBehaviour
 {
+    // Init
+    private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		coroutine = CheckMovement();
+		StartCoroutine(coroutine);
+	}
+
+    // Check if spawner has spawned egg in such a way that it is falling into space - if so destroy the egg
+	private IEnumerator CheckMovement()
+	{
+		while (true)
+		{
+			if (rb.velocity.magnitude < 0.01f && Time.timeScale != 0f)
+			{
+				yield return new WaitForSeconds(2f);
+				if (rb.velocity.magnitude < 0.01f)
+				{
+					Debug.Log("Object is stationary.");
+					StopCoroutine(coroutine);
+				}
+			}
+			else
+			{
+				Debug.Log("Object is moving, destroying...");
+				Destroy(gameObject);
+			}
+			yield return null;
+		}
+	}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
